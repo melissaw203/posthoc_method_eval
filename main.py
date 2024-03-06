a89a89b27472f5aee468bb47043f8fd6c3f43354
melissaw203 <melissaw203@gmail.com>
2024-03-06 12:07:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+import streamlit as st
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # Use a non-interactive backend
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Creating a sample 3D plot
+st.title('3D Plot in Streamlit')
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Create data
+X = np.linspace(-5, 5, 100)
+Y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(X, Y)
+Z = np.sin(np.sqrt(X**2 + Y**2))
+
+# Plot the surface
+ax.plot_surface(X, Y, Z, cmap='viridis')
+
+# Show the plot in Streamlit
+st.pyplot(fig)
+
 
 
 # Creating a sample 3D plot
