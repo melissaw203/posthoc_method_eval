0229014bb5ab27e7b005d3dfff9a240d4f2f361e
melissaw203 <melissaw203@gmail.com>
2024-03-06 11:50:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+
+
+# Creating a sample 3D plot
+st.title('3D Plot in Streamlit')
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Create data
+X = np.linspace(-5, 5, 100)
+Y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(X, Y)
+Z = np.sin(np.sqrt(X**2 + Y**2))
+
+# Plot the surface
+ax.plot_surface(X, Y, Z, cmap='viridis')
+
+# Show the plot in Streamlit
+st.pyplot(fig)
+
 st.write("Randomly Generated Time Series Data")
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
